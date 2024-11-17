a53ab2493fd3c6a5564a689bb1b7bedcd32fe814
Hanz <2409281700@qq.com>
2024-11-17 18:22:30 +0800

    12121

--- src/base_error_declare.h
+++
@@ -1,8 +0,0 @@
-bool myAssert(bool b, int row, char errType);
-void error_a(const string &input, int &i, string &value, int row);
-bool error_b(string name, int row);
-void error_i(Node *root);
-void error_j(Node *root);
-void error_k(Node *root);
-
-set<pair<int, char>> errors;
